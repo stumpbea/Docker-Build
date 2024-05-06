d05fba3f4c59f5a854891b4710764db84160c73a
stumpbea <stumpbea@students.zhaw.ch>
2024-05-06 13:13:06 +0200

    WI

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello FS2024 DevOps Course!!! :-)');
+    res.send('Hello WI FS2024 DevOps Course!!! :-)');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
